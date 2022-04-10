41212546c79c91df3fe9b4a742859531846720d9
SoftLeaderGy <826044698@qq.com>
2022-04-10 17:49:35 +0800

    初始化工程

---
+++ gy-chat-room/src/main/java/com/yang/gychatroom/GyChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.yang.gychatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyChatRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyChatRoomApplication.class, args);
+    }
+
+}
---
+++ gy-chat-room/src/test/java/com/yang/gychatroom/GyChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.gychatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyChatRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
